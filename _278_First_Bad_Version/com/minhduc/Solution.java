87b4a93ade4f71d3a1b4c4c82f18dd2f6f02fc57
MinhDuc0602 <pminhduc62110@gmail.com>
2022-07-15 12:43:35 +0700

    commit first bad version

--- _278_First_Bad_Version/com/minhduc/Solution.java
+++ _278_First_Bad_Version/com/minhduc/Solution.java
@@ -4,12 +4,17 @@ public class Solution extends VersionControl{
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        int firstBadVersion = 0;
         while(left <= right) {
             int pivot = (left+right)/2;
-            
+            if(!isBadVersion(pivot)) {
+                left = pivot + 1;
+            } else {
+                if(!isBadVersion(pivot - 1)) {
+                    return pivot;
+                }
+                right = pivot - 1;
+            }
         }
-
-        return -1;
+        return 1;
     }
 }
--- _278_First_Bad_Version/com/minhduc/VersionControl.java
+++ _278_First_Bad_Version/com/minhduc/VersionControl.java
@@ -1,5 +1,7 @@
 package com.minhduc;
 
 public class VersionControl {
-
+    public boolean isBadVersion(int version) {
+        return false;
+    }
 }
